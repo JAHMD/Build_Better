9fbf607974af9a78e609618430d8fb4fe0ab73bc
Ahmed Wael <awael3977@gmail.com>
2023-04-07 22:59:47 +0200

    Update home page's styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,18 +26,19 @@ function Home() {
 	return (
 		<section className="rounded-lg h-full p-6 md:p-8 bg-primary-brown-200-30 shadow-md">
 			<div className="h-full rounded-lg p-4 flex flex-col justify-center items-center gap-4 bg-primary-brown-100-90 text-center">
-				<h1 className="text-3xl font-bold">Welcome to Build Better</h1>
-				<p className="max-w-md">
+				<h1 className="text-2xl sm:text-3xl font-bold">
+					Welcome to Build Better
+				</h1>
+				<p className="max-w-md text-sm sm:text-base">
 					We're here to help you get things done and achieve your goals. With
 					our customizable task manager, note-taking tools, Pomodoro timer,
 					you'll be able to build better habits and reach new heights.
 				</p>
 				<NavLink
 					to="tasks/all"
-					className="btn btn-primary px-4 w-fit flex justify-between items-center gap-2 group"
+					className="btn btn-primary px-4 w-fit flex justify-between items-center gap-2"
 				>
-					Get Started{" "}
-					<MdArrowRightAlt className="text-xl group-hover:animate-bounce" />
+					Get Started <MdArrowRightAlt className="text-xl animate-bounce" />
 				</NavLink>
 			</div>
 		</section>
